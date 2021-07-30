6faff72fc311be4112ad14373c2e1ff988832635
Ricardo Montenegro <ramn21@gmail.com>
2021-07-30 03:00:58 +0200

    Add MockData for test

---
+++ tools/MockData.js
@@ -0,0 +1,43 @@
+const MockData = {
+  data: [
+    {
+      id: 1,
+      type: 'users',
+      attributes: {
+        name: 'test1',
+        lastname: 'data1',
+        avatar: '/mock.data.com/test1.png',
+        email: 'test1@testdata.com',
+        job: 'tester',
+        phone: '+123456789'
+      }
+    },
+    {
+      id: 2,
+      type: 'users',
+      attributes: {
+        name: 'test2',
+        lastname: 'data2',
+        avatar: '/mock.data.com/test2.png',
+        email: 'test2@testdata.com',
+        job: 'testeador',
+        phone: '+124681012'
+      }
+    },
+    {
+      id: 3,
+      type: 'users',
+      attributes: {
+        name: 'test3',
+        lastname: 'data3',
+        avatar: '/mock.data.com/test3.png',
+        email: 'test3@testdata.com',
+        job: 'testeando',
+        phone: '+1135791113'
+      }
+    },
+    
+  ],
+};
+
+export default MockData;
