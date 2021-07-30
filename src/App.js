dead993c80216c341d70bd4db59d25b8f44a4942
Ricardo Montenegro <ramn21@gmail.com>
2021-07-30 18:04:41 +0200

    Add components Loading and Errors to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Display from './components/Display';
 import useFetch from './customHook/useFetch';
+import Loading from './components/Loading';
 import Style from './Styles/App.module.css';
+import Errors from './components/Errors';
 
 const url = 'http://localhost:4000/api/v1/users'
 const App = () => {
   const {loading, data, error} = useFetch(url);
-  if (loading) return 'Loading';
-  if (error) return 'Something went wrong'
+  if (loading) return (<Loading />);
+  if (error) return (<Errors />);
   return (
     <div className={Style.container_app}>
       {data && <Display users={data} />}
