a3d622596bd240aee4f5df6ca21b98a76fbe7f75
Ricardo Montenegro <ramn21@gmail.com>
2021-07-30 02:25:56 +0200

    Add MaterialIcon and Search.module.css

--- src/components/SearchUser.js
+++ src/components/SearchUser.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import TextField, {HelperText, Input} from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 import '@material/react-text-field/dist/text-field.css';
+import '@material/react-material-icon/dist/material-icon.css';
+import Style from '../Styles/Search.module.css';
 
 const SearchUser = ({term, handleSearch}) => {
   return (
-    <div style={{width: '100%'}}>
+    <div className={`w100 ${Style.sticky}`}>
       <TextField
         label='Search User'
         helperText={<HelperText>ie. Max, Stainer, Samantha</HelperText>}
-        style={{width: '100%'}}
+        className='w100'
+        trailingIcon={<MaterialIcon icon="search"/>}
       >
         <Input value={term} onChange={handleSearch} />
       </TextField>
